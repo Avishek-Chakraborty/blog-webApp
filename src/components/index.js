a2cc61fcca8ea808bdf4cb7f633f2acf9a806380
Avishek Chakraborty <avishek3141@gmail.com>
2024-02-07 09:28:14 +0530

    version 5.2 Basic react hook form configuration done

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,23 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container  from "./container/Container";
-import  Logo  from "./Logo";
-import LogoutBtn from './Header/LogoutBtn'
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
 
-export{
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn
-}
+export {
+	Header,
+	Footer,
+	Container,
+	Logo,
+	LogoutBtn,
+	Button,
+	Input,
+	Login,
+	PostCard,
+	Select,
+};
