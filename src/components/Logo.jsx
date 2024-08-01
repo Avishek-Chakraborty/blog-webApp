d5f476d25dd0068b5e42ceccbf45ac031c8bafc2
Avishek Chakraborty <avishek3141@gmail.com>
2024-08-01 15:31:02 +0530

    logo fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 
 function Logo({ width = "100px" }) {
-	return <div className="text-white" style={{ width }}>Logo</div>;
+	return <div className="text-white" style={{ width }}>
+		<img src="src\assets\favicon.ico"/>
+	</div>;
 }
 
 Logo.propTypes = {
